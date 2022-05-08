db5071fde747dea901ee2246d4fdaa1584a4fd46
marquez201 <richard100marquez@gmail.com>
2022-05-07 21:23:13 -0600

    1

--- app/src/main/java/com/example/granpahelpasistent/MainActivity.java
+++ app/src/main/java/com/example/granpahelpasistent/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.granpahelpasistent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btn_iniciar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn_iniciar = (Button) findViewById(R.id.button_1);
+        btn_iniciar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, Login.class);
+                startActivity(i);
+            }
+        });
     }
 }
